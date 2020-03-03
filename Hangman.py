22042a76e7e07d8ab648f4cafd99dc076cf87309
Aringupta <arinvgupta@gmail.com>
2020-03-03 10:05:47 -0500

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -11,6 +11,36 @@ def initialize():
    
     print(updatedWord)
     print SECRET
+    
+    val=0
+
+def test():
+
+    global val
+
+    secret = "BALOON"
+
+    updatedword = ['_','_','_','_','_','_','_']
+
+    for i in range (0, len(Secret)):
+
+        print (updatedWord[1])
+
+    Letter = str(input("What is your guess: "))
+
+    val = Secret.find (Letter)
+
+    del updatedWord[val]
+
+    updatedWord.insert(val, Letter)
+
+    for i in range (0, len(Secret)):
+
+        print(updatedWord[i])
+
+    if Secret.find(Letter) ==-1
+
+        return "Wrong Letter"
 
 def getLetter():
     global letter
